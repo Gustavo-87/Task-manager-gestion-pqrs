85cbf5d6b0766300185839969a413acfcd2c9f17
Gustavo-87 <Guadpilo87@hotmail.com>
2026-09-14 14:35:02 -0500

    Actualizacion de las vistas en pestaña

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{ config('app.name', 'Gestión de PQRS') }}</title>
+        <link rel="icon" type="image/jpeg" href="{{ asset('images/logo-gestion-pqrs.jpg') }}">
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700&display=swap" rel="stylesheet" />
         @vite(['resources/css/app.css', 'resources/js/app.js'])
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Gestión de PQRS') }}</title>
+        <link rel="icon" type="image/jpeg" href="{{ asset('images/logo-gestion-pqrs.jpg') }}">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
